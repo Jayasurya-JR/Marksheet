e10c99dab3e173e29122ec8965a398048ddf7e90
Jayasurya-JR <gjsurya.73@gmail.com>
2023-07-07 10:42:42 +0530

    sortable table

--- script.js
+++ script.js
@@ -48,24 +48,7 @@ const myJSON =`{
     ]
   }`;
   
-
-  const myObject = JSON.parse(myJSON);
-
-//   let object1 = myObject.result[0]["name"];
-//   document.getElementById("demo").innerHTML = object1;
-
-// let student = "<ul> ";
-
-// for (let i = 0; i < myObject.result.length; i++) {
-//     student += '<li>' + myObject.result[i]["marks"]["physics"] + "</li>";
-    
-// }
-// '</ul>'
-// document.getElementById("demo").innerHTML = student;
-// console.log(student);
-
-
-
+ const myObject = JSON.parse(myJSON);
 
 let markTable = "<tbody>"
 
@@ -77,7 +60,59 @@ for (let i = 0; i < myObject.result.length; i++) {
                    <td>${myObject.result[i]["marks"]["chemistry"]}</td>
                    <td>${myObject.result[i]["marks"]["physics"]}</td>
                </tr>`
-
 }
 "</tbody>"
 document.getElementById("myTable").innerHTML = markTable;
+
+function defaultOrder() {
+  document.getElementById("myTable").innerHTML = markTable;
+}
+
+//function to display table in ascending order
+
+function ascendingOrder(){
+
+  const ascendArray = myObject.result.sort((a,b)=>{
+    if(a.name < b.name) {
+      return -1
+    }
+  });
+
+let ascentTable = "<tbody>"
+
+for (let i = 0; i < ascendArray.length; i++) {
+  ascentTable +=`<tr>
+                   <td>${ascendArray[i]["name"]}</td>
+                   <td>${ascendArray[i]["marks"]["math"]}</td>
+                   <td>${ascendArray[i]["marks"]["english"]}</td>
+                   <td>${ascendArray[i]["marks"]["chemistry"]}</td>
+                   <td>${ascendArray[i]["marks"]["physics"]}</td>
+               </tr>`
+}
+"</tbody>"
+document.getElementById("myTable").innerHTML = ascentTable;
+}
+
+//function to display table in descending order
+function descendingOrder(){
+
+  const descendArray = myObject.result.sort((a,b)=>{
+    if(a.name > b.name) {
+      return -1
+    }
+  });
+
+let descentTable = "<tbody>"
+for (let i = 0; i < myObject.result.length; i++) {
+  descentTable +=`<tr>
+                   <td>${descendArray[i]["name"]}</td>
+                   <td>${descendArray[i]["marks"]["math"]}</td>
+                   <td>${descendArray[i]["marks"]["english"]}</td>
+                   <td>${descendArray[i]["marks"]["chemistry"]}</td>
+                   <td>${descendArray[i]["marks"]["physics"]}</td>
+               </tr>`
+}
+"</tbody>"
+document.getElementById("myTable").innerHTML = descentTable;
+}
+
